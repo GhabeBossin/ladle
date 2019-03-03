ca9d47ce8c1ed5cdb71e7eeaf08fcc4f16cffe0b
joshua <josh@joshuadennis.ca>
2019-03-03 00:35:58 +0000

    Added all necessary seed files

---
+++ db/seeds/7_user_achievements.js
@@ -0,0 +1,20 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('user_achievements').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('user_achievements').insert([
+        { achievements_id: 1, user_id: 1 },
+        { achievements_id: 2, user_id: 2 },
+        { achievements_id: 2, user_id: 3 },
+        { achievements_id: 1, user_id: 3 },
+        { achievements_id: 1, user_id: 4 },
+        { achievements_id: 2, user_id: 4 },
+        { achievements_id: 3, user_id: 4 },
+        { achievements_id: 1, user_id: 5 },
+        { achievements_id: 2, user_id: 6 },
+        { achievements_id: 1, user_id: 7 },
+      ]);
+    });
+};

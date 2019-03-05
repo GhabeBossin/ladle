f3b6a5d1cda3980d26a9bd574d46b89070a88127
Ghabe Bossin <gee.bossin@gmail.com>
2019-03-05 03:07:17 +0000

    App.js fix merge conflict nonsense inserts

--- src/client/App.js
+++ src/client/App.js
@@ -1,27 +1,5 @@
 import React, { Component } from 'react';
 import './app.css';
-<<<<<<< cb325686559e7fa8828d4f4901faf900878c35ca
-<<<<<<< 55e65f5f982efc853666578a97de5936c44c4fc7
-import ReactImage from './react.png';
-import Nav from './Nav';
-import SignUp from './SignUp';
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      };
-  }
-  state = { username: null };
-
-  componentDidMount() {
-  //   fetch('/api/getUsername')
-  //     .then(res => res.json())
-  //     .then(user => this.setState({ username: user.username }));
-=======
-// import ReactImage from './react.png';
-=======
->>>>>>> footer text added
 import Nav from './Nav'
 import Game from './Game'
 import Footer from './Footer'
@@ -33,7 +11,6 @@ export default class App extends Component {
     // fetch('/api/getUsername')
     //   .then(res => res.json())
     //   .then(user => this.setState({ username: user.username }));
->>>>>>> Main game setup
   }
 
   render() {
@@ -41,16 +18,8 @@ export default class App extends Component {
     return (
       <div className="app">
         <Nav />
-<<<<<<< 55e65f5f982efc853666578a97de5936c44c4fc7
-        <SignUp />
-=======
         <Game />
-        {/* <MainContent /> */}
-        {/* {username ? <h1>{`3 Blind Mice is using  ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" /> */}
         <Footer />
-
->>>>>>> Main game setup
       </div>
     );
   }

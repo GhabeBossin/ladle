859a562d2fc4c7c6a8c663a146719b67c3db4531
Ghabe Bossin <gee.bossin@gmail.com>
2019-03-05 00:22:34 +0000

    Main game setup

--- src/client/App.js
+++ src/client/App.js
@@ -2,24 +2,28 @@ import React, { Component } from 'react';
 import './app.css';
 // import ReactImage from './react.png';
 import Nav from './Nav'
+import Game from './Game'
 
 export default class App extends Component {
-  state = { username: null };
+  // state = { username: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    // fetch('/api/getUsername')
+    //   .then(res => res.json())
+    //   .then(user => this.setState({ username: user.username }));
   }
 
   render() {
     // const { username } = this.state;
     return (
-      <div className="App">
+      <div className="app">
         <Nav />
+        <Game />
+        {/* <MainContent /> */}
         {/* {username ? <h1>{`3 Blind Mice is using  ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
         <img src={ReactImage} alt="react" /> */}
-        
+        {/* <Footer /> Goes Here*/}
+
       </div>
     );
   }

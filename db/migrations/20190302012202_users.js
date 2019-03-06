1e475c3e4317f6b5cfea385222cf39f30c42f1e8
joshua <josh@joshuadennis.ca>
2019-03-06 05:23:41 +0000

    Added last name and changed passwords in users table

--- db/migrations/20190302012202_users.js
+++ db/migrations/20190302012202_users.js
@@ -2,7 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments();
-    table.string('name');
+    table.string('first_name');
+    table.string('last_name');
     table.string('password');
     table.string('email');
     table.string('group');

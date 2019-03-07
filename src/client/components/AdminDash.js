6364d96504efcf611a67e8f3e59515f8d00a5548
Ghabe Bossin <gee.bossin@gmail.com>
2019-03-07 19:45:15 +0000

    StyledCardButton bug?

--- src/client/components/AdminDash.js
+++ src/client/components/AdminDash.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Container } from 'reactstrap'
+import AdminNav from './AdminNav'
 
 class AdminDash extends Component {
   render() {
     return (
       <Container>
+        <AdminNav />
         <h1>I'm the Admin Dashboard!</h1>
       </Container>
     )

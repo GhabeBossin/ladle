d0fd8658be8f0fd4bbb38b33b8ebd3707cff8a99
joshua <josh@joshuadennis.ca>
2019-03-12 19:32:35 +0000

    Still trying to fix git merge

--- src/client/components/admin/UserDelete.js
+++ src/client/components/admin/UserDelete.js
@@ -2,20 +2,11 @@ import React, { Component } from 'react'
 import { 
   Card,
   CardText, 
-<<<<<<< HEAD
   Button,
   Container } from 'reactstrap'
 import { 
   ModalContainer, 
-  ModalWrapper, 
-  BtnDiv,
-  DelBtn} from '../../styles/adminStyles';
-=======
-  Button } from 'reactstrap'
-import { 
-  ModalContainer, 
   ModalWrapper } from '../../styles/adminStyles';
->>>>>>> parent of 7e9c489... added seeds
 
 class UserDelete extends Component {
   constructor({deleteUser, data}) {
@@ -42,21 +33,8 @@ class UserDelete extends Component {
         <ModalContainer>
           <ModalWrapper>
             <Container>
+              <Card>
               <Button close onClick={this.buttonToggle}/>
-<<<<<<< HEAD
-                <h3>Are You Sure?</h3>
-                <CardText>This action cannot be undone!</CardText>
-                <BtnDiv>
-                  <Button onClick={this.buttonToggle}>Cancel</Button>
-                  <DelBtn 
-                    onClick={() => {
-                    this.deleteUser(this.state)
-                    this.buttonToggle() }}> 
-                    Delete User 
-                  </DelBtn>
-                </BtnDiv>
-            </Container>
-=======
               <h3>Are You Sure?</h3>
               <CardText>This action cannot be undone!</CardText>
               <Button onClick={this.buttonToggle}>Cancel</Button>
@@ -68,7 +46,7 @@ class UserDelete extends Component {
                 Delete User 
               </Button>
             </Card>
->>>>>>> parent of 7e9c489... added seeds
+            </Container>
           </ModalWrapper>
         </ModalContainer>
       )}

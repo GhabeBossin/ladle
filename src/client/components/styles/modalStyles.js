1004e0b187e741bda7e0d4835dad7cb3da4dd8bc
Ghabe Bossin <gee.bossin@gmail.com>
2019-03-14 01:12:00 +0000

    fuzzy fix

--- src/client/components/styles/modalStyles.js
+++ src/client/components/styles/modalStyles.js
@@ -11,26 +11,24 @@ const ModalContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   background-color: rgba(0, 0, 0, 0.5);
-  min-width: 100%;
-  min-height: 100%;
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 3;
-  padding-top: 25vh;
+  min-width: 100%;
+  min-height: 100%;
+  z-index: 2;
 `;
 
 const ModalWrapper = styled.div`
   display: flex;
   flex-direction: column;
   background-color: white;
-  min-width: fit-content;
-  max-width: 60vw;
+  max-width: 60%;
   overflow: scroll;
-  z-index: 4;
   border-radius: 5px;
-  padding: .2em 1.5em 1.5em;
+  padding: 1em;
 `;
 
 export { 
